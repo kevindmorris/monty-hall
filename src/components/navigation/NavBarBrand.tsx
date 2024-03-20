116fabdf26c8163173da6dd14157797f6aa71018
Morris, Kevin <Kevin.Morris@noblis.org>
2024-03-20 17:07:51 -0400

    Test commit

--- src/components/navigation/NavBarBrand.tsx
+++ src/components/navigation/NavBarBrand.tsx
@@ -2,8 +2,8 @@ import { NavLink } from "react-router-dom";
 
 export default function NavBarBrand() {
   return (
-    <NavLink to="/" style={{ height: 30 }}>
-      <img src="/logo-full.png" alt="Logo" style={{ height: 30 }} />
+    <NavLink to="/" style={{ height: 32 }}>
+      <img src="/logo-full.png" alt="Logo" style={{ height: 32 }} />
     </NavLink>
   );
 }
