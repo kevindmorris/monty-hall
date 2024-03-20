023b3e489f23195e21d06500cb0e25143ac588f0
Morris, Kevin <Kevin.Morris@noblis.org>
2024-03-20 17:42:24 -0400

    changes

--- src/components/navigation/NavBarBrand.tsx
+++ src/components/navigation/NavBarBrand.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 export default function NavBarBrand() {
   return (
     <NavLink to="/" style={{ height: 32 }}>
-      <img src="/logo-full.png" alt="Logo" style={{ height: 32 }} />
+      <img src="/monty-hall/logo-full.png" alt="Logo" style={{ height: 32 }} />
     </NavLink>
   );
 }
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,15 +2,18 @@ import { createBrowserRouter } from "react-router-dom";
 import { AboutPage, GamePage } from "../pages";
 import Layout from "../components/layout";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { index: true, element: <GamePage /> },
-      { path: "about", element: <AboutPage /> }
-    ]
-  }
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <GamePage /> },
+        { path: "about", element: <AboutPage /> }
+      ]
+    }
+  ],
+  { basename: "/monty-hall" }
+);
 
 export default router;
