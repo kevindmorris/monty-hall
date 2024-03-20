8e37d1a29777e1b7c9711114d792c79d85fcfa31
Morris, Kevin <Kevin.Morris@noblis.org>
2024-03-20 16:32:32 -0400

    test husky

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -13,9 +13,6 @@ export default function Layout() {
           flexDirection: "column"
         }}
       >
-
-
-        
         <Outlet />
       </main>
     </React.Fragment>
--- src/lib/hooks/index.ts
+++ src/lib/hooks/index.ts
@@ -1,2 +1,1 @@
 export { useAppDispatch, useAppSelector } from "./hooks";
-
